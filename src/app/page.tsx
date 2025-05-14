cf1afbcd75221a361281835b23089f69b8b7d711
ridoy <ridoy51306@gmail.com>
2025-05-14 17:55:54 -0400

    Add console log and fix typo in instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  console.log("Hello from the console!");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -16,9 +18,8 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              src/app/page.tsx.
             </code>
-            .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
